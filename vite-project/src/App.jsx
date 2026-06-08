dbe21bb462f886959ea82476953bfa7ae84f2560
Evelin Cobos <evecobos8@gmail.com>
2026-06-07 21:42:22 -0400

    to do - change from style to a class

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 
   const languages = ['English','Spanish', 'Haitian Creole', 'Portuguese', 'French', 'Japanese']
@@ -17,17 +15,18 @@ import './App.css'
     return <h1 style={{fontSize: `${size}px`}}>{greetings[language]}</h1>
   }
 
-  const LanguageButtons = ({language, setLanguage}) => {
+  const LanguageButtons = ({language, setLanguage, theme}) => {
     return (
       <>
         {languages.map((lang) => (
           <button 
+          className={theme && language !== lang ? "theme" : undefined}
           key={lang} 
           onClick={() => setLanguage(lang)}
           style={{
             backgroundColor: language === lang ? 'blue' : 'white',
             color: language === lang ? 'white' : 'black'
-          }}
+          }} //turn this into a class then do a ternary so that on dark mode these buttons when not clicked they are black background - white text
           >{lang}
           </button> 
         ))}
@@ -46,10 +45,24 @@ import './App.css'
   }
 
 
-  const ThemeToggle = () => {
+  const ThemeToggle = ({theme, changeTheme}) => {
     return (
       <>
-      <button></button>
+      <button 
+      className={theme ? "theme" : undefined}
+      onClick={changeTheme}
+      // style={{
+      // backgroundColor: theme ? 'black' : 'white',
+      // color: theme ? 'white' : 'black'
+      // }}
+      >{theme ? 'Switch to Dark Mode' : 'Switch to Light Mode'}
+      </button>
+      <body
+      style={{
+      backgroundColor: theme ? 'black' : 'white',
+      color: theme ? 'white' : 'black'
+      }}
+      ></body>
       </>
     )
   }
@@ -57,17 +70,23 @@ import './App.css'
 function App() {
 
   const [language, setLanguage] = useState('English');
-  const [size, setSize] =useState(32);
+  const [size, setSize] = useState(32);
+  const [theme, setTheme] = useState(false)
 
   const increase = () => {setSize(size + 1)}
   const decrease = () => {setSize(size - 1)}
 
+  const changeTheme = () => {
+    setTheme((curr) => !curr)
+  }
+
 
   return (
     <>
-    <SizeButtons increase={increase} decrease={decrease}/>
-    <GreetingDisplay language={language} size={size}/>
-    <LanguageButtons language={language} setLanguage={setLanguage}/>
+      <ThemeToggle theme={theme} changeTheme={changeTheme}/>
+      <SizeButtons increase={increase} decrease={decrease}/>
+      <GreetingDisplay language={language} size={size}/>
+      <LanguageButtons language={language} setLanguage={setLanguage} theme={theme}/>
     </>
   )
 }
